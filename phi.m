dab5bd8398ff04015068c47b8f23409d5b09d599
MElemental <34526233+MElemental@users.noreply.github.com>
2019-05-26 08:03:58 +0000

    Added documentation

--- phi.m
+++ phi.m
@@ -1,7 +1,10 @@
 function p = phi(x, y, z, epsilon)
 % PHI This function calculates the fitness function of x_1(x), x_2(y), and x_3(z).
-%   fitness functions derived from the Payoff matrix in Toupo and Strogatz 2015.
-%   Called by the xdot and ydot functions.
+%   x, y, and z refer to to population densities.
+%   epsilon refers to the magnitude of loss in a non-zero sum game.
+%   fitness functions derived from the payoff matrix in Toupo and Strogatz 2015.
+%   p refers to the wighted average of the funtions, phi.
+%   Called by the systems of differential equations, xdot and ydot, functions.
     fitnessX = -(epsilon + 1)*y + 1*z;
     fitnessY = 1*x + -(epsilon + 1)*z;
     fitnessZ = -(epsilon + 1)*x + y;
