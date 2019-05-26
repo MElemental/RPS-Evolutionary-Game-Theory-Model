af89ce86ba37af410becb9a6aa3200322da93fd6
MElemental <34526233+MElemental@users.noreply.github.com>
2019-05-26 07:54:44 +0000

    Update phi.m

--- phi.m
+++ phi.m
@@ -1,4 +1,7 @@
 function p = phi(x, y, z, epsilon)
+% PHI This function calculates the fitness function of x_1(x), x_2(y), and x_3(z).
+%   fitness functions derived from the Payoff matrix in Toupo and Strogatz 2015.
+%   Called by the xdot and ydot functions.
     fitnessX = -(epsilon + 1)*y + 1*z;
     fitnessY = 1*x + -(epsilon + 1)*z;
     fitnessZ = -(epsilon + 1)*x + y;
